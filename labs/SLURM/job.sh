328995d75ce829ee8fa0a9b9c70786536d6cc2ed
Fausto Carcassi <fausto.carcassi@gmail.com>
2025-03-05 18:30:12 +0100

    Update job.sh

--- labs/SLURM/job.sh
+++ labs/SLURM/job.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 #Set job requirements
+#SBATCH --gpus=1
 #SBATCH -n 16
 #SBATCH -t 5:00
 
 module load 2024
+conda deactivate
 module load Anaconda3/2024.06-1
+conda activate arccourse
+
+# At this point I was getting the following error:
+# ImportError: /lib64/libstdc++.so.6: version `GLIBCXX_3.4.30' not found (required by /home/carcassi/.conda/envs/arccourse/lib/python3.11/site-packages/zmq/backend/cython/../../../../../libzmq.so.5)
+# This can be fixed loading the compiler:
+module load GCC/13.3.0
+
+# This is because otherwise UNSLOTH does not recognize the name of the GPU
+# so we refer to it as cuda:0
+export CUDA_VISIBLE_DEVICES=0
+
